9e4280cba663bbb9079605e2e01d57bc221955ec
Poop Poop <rosilescarlos28@gmail.com>
2021-02-02 23:47:51 +0000

    rfrfere

--- functions.js
+++ functions.js
@@ -1,12 +1,87 @@
+const admin = require('firebase-admin');
+const db = admin.firestore();
+db.settings({ ignoreUndefinedProperties: true })
+
 function appendKey(dic, value) {
 	var lengthh = Object.keys(dic).length
-	var size = lengthh + 1
+	var size = lengthh + 50
 	dic[size] = value
-}//============================================================================================
-exports.appendImage = async function f(collection, cardname) {
-	const cityRef = db.collection(collection).doc("cards");
+}
+//============================================================================================
+exports.appendImage = async function f(documentt, imagename) {
+	const cityRef = db.collection("PDUsers").doc(documentt);
 	const doc = await cityRef.get();		
-	var attendees = doc.data().object;
-	appendKey(attendees, cardname);
-	setData(collection, "cards", attendees)
+	var attendees = doc.data().images;
+	appendKey(attendees, imagename);
+	await cityRef.update({
+		images: attendees
+	})
+}
+async function setData(collectionn, document, object) {
+	const docRef = db.collection(collectionn).doc(document);
+	await docRef.set({
+		object
+	});
+}
+exports.retrieveImages = async function getCVV(documenT) {
+	const snapshot = await db.collection('PDUsers').doc(documenT)
+	const doc = await snapshot.get();
+	return doc.data().images
+}
+
+const FieldValue = admin.firestore.FieldValue;
+exports.deleteImage = async function findNumber(documenT, image) {
+  var ref = db.collection('PDUsers').doc(documenT);
+	const doc = await ref.get();
+	for (var i=0; i < 10000; i++) {
+		  var images = doc.data().images
+			var poop = images[i]
+			var update = ('images.' + i)
+			console.log(images)
+			console.log(poop + " Is being checked")
+			console.log(i + " Times attempted")
+			console.log(image + " Is User input")
+			if (poop == image) {
+				await ref.update({
+					[update]: FieldValue.delete()
+				})
+				console.log("Image Deleted Successfully!")
+				break;
+			}
+			else if (images.i == undefined) {
+				console.log('Checking...')
+				continue;
+			}
+			else {
+				console.log('Checking...')
+				continue;
+			}
+		}
+	}
+exports.renameImage = async function findNumberR(documenT, imageName, newName) {
+  var ref = db.collection('PDUsers').doc(documenT);
+	const doc = await ref.get();
+	for (var i=0; i < 10; i++) {
+		  var images = doc.data().images
+			var poop = images[i]
+			var update = ('images.' + i)
+      console.log(update)
+			if (poop == imageName) {
+				await ref.update({
+					[update]: newName
+				})
+				console.log("Image renamed Successfully!")
+				break;
+			}
+			else if (images.i == undefined) {
+				continue;
+			}
+			else {
+				continue;
+			}
+		}
+	}
+
+exports.randint = function(min, max) {
+	return Math.floor(Math.random() * (max - min)) + min;
 }
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const {Storage} = require('@google-cloud/storage');
 const express = require("express");
 const app = new express();
+app.locals.data = '';
 const fileUpload = require('express-fileupload');
 const cors = require('cors');
 const morgan = require('morgan');
@@ -9,9 +10,8 @@ var admin = require("firebase-admin");
 var fs = require('fs');
 var serviceAccount = require("./key.json");
 const bodyParser = require('body-parser');
-var functions = require('./functions')
-//Requiring Modules Above
 
+//Requiring Modules Above
 //Initalizing server and Firebase app below
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -20,6 +20,7 @@ admin.initializeApp({
 });
 var bucket = admin.storage().bucket();
 const db = admin.firestore();
+var functions = require('./functions')
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 app.use(express.static('views'));
@@ -31,10 +32,16 @@ app.use(fileUpload({
   createParentPath: true
 }));
 app.get('/', function(req, res) {
-	res.render('index', {message:""})
+	res.render('index', {imageMessage:""})
+})
+app.get('/faq', function(req, res) {
+	res.render('faq', {imageMessage:""})
+})
+app.get('/join', function(req, res) {
+	res.render('join', {imageMessage:""})
 })
 app.get('/seeImages', function(req, res) {
-	res.render('see', {imageLink:"https://breakthrough.org/wp-content/uploads/2018/10/default-placeholder-image.png"})
+	res.render('see', {images:"", imageLink:"/images/pixel.png"});
 })
 app.get('/createAccount', function(req, res) {
 	res.render('create', {message:""})
@@ -42,16 +49,22 @@ app.get('/createAccount', function(req, res) {
 app.get('/signIn', function(req, res) {
 	res.render('log', {message:""})
 })
+app.get('/delete', function(req, res) {
+res.render('delete', {message:"", images:""})
+})
+app.get('/accounts', function(req, res) {
+	res.render('other', {message:""})
+})
+app.get('/rename', function(req, res) {
+	res.render('rename', {message:"", images:""})
+});
 const storage = new Storage({
   keyFilename: './key.json',
 });
-const port = process.env.PORT || 3000;
+var port = process.env.PORT || functions.randint(1000, 9999);
 app.listen(port, () => 
   console.log(`App is listening on port ${port}.`)
 );
-
-//Download below
-
 
 /*
 =======================================================================================================
@@ -59,6 +72,65 @@ app.listen(port, () =>
 =======================================================================================================
 */
 /*=======================================Post requests=============================================*/ 
+app.post('/create', async (req, res) => {
+	var userData = {
+		username: req.body.username,
+		password: req.body.password,
+		confirmPassword: req.body.confirmPassword
+	}
+	var uploadOptions = {
+		destination: bucket.file(userData.username + "/" + 'start.png'),
+		resumable: false		
+	}
+	await bucket.upload('./start.png', uploadOptions, function(err, file) {console.log('start.png uploaded')})
+	if (userData.password != userData.confirmPassword) {
+res.render('create', {message:"The password's do not match! Pleaase try again."})
+	
+	}
+	else if (userData.password == userData.confirmPassword) {
+	var object = {
+		username: req.body.username,
+		password: req.body.password,
+		images: {
+			0:""
+		}
+	}
+	db.collection('PDUsers').doc(userData.username).set(object);
+	res.redirect('/')
+	}
+})
+
+app.post('/logIn', async (req, res) => {
+	var username = req.body.username;
+	req.app.locals.username = username;
+	var password = req.body.password;
+ 	var ref = await db.collection('PDUsers').doc(username);
+ 	var doc = await ref.get();
+	if (password == doc.data().password && doc.exists) {
+		res.render('index', {imageMessage:""})
+	}
+	else {
+		res.render('log', {message:"Incorrect username or password!"})
+	}
+
+	app.post('/image', async (req, res) => {
+		var imageName = req.body.imageName
+		console.log(req.app.locals.username + " Is viewing images")
+		let bucketName = 'gs://poopnet-4fb22.appspot.com'
+		var filename = imageName
+		var downloadFile = async() => {
+			let destFilename = './views/images/image.png';
+			var options = {
+				destination: destFilename
+			};
+			await storage.bucket(bucketName).file(req.app.locals.username + '/' + filename).download(options);
+			var g = await functions.retrieveImages(req.app.locals.username)
+			res.render('see', {images:Object.keys(g).map(k => g[k]).join('<br><br>'), imageLink:'images/image.png'});
+		} 
+	downloadFile();
+	})
+
+
 app.post('/upload-avatar', async (req, res) => {
     try {
         if(!req.files) {
@@ -68,70 +140,58 @@ app.post('/upload-avatar', async (req, res) => {
             });
         }
 				else {
-					  const options = {
-							destination: bucket.file("poop/caca.png"),
+					var avatar = req.files.avatar;
+					  var options = {
+							destination: bucket.file(req.app.locals.username + "/" + avatar.name),
 							resumable: false
 						}
-            let avatar = req.files.avatar;
             avatar.mv('./uploads/' + avatar.name);	
 						async function e() {
-						console.log((avatar.size / 1e+6) + " MB is the size of the file uploaded")
 						var path = 'uploads/' + avatar.name
-						await bucket.upload(path, options, function(err, file) {console.log("File uploaded")})
-
+						await bucket.upload(path, options, function(err, file) {console.log("File uploaded")});
+						functions.appendImage(req.app.locals.username, avatar.name);
 						fs.unlink('uploads/' + avatar.name, function (err) {if (err) throw err; console.log('File deleted!');});
 						}
 						e();	
-						res.render('index', {message:"File uploaded successfuly!"})
+						res.render('index', {imageMessage:"File uploaded successfuly!"})
         }
     } catch (err) {
         res.status(500).send(err);
 				console.log("Error status 500")
 				if (avatar.size < (2e+6)) {
-					res.render('index', {message: "The file size is over 2MB! Please choose an image that is less than 2 Megabytes."})
+					res.render('index', {imageMessage: "The file size is over 2MB! Please choose an image that is less than 2 Megabytes."})
 				} 
     }
 
 });
-app.post('/image', async (req, res) => {
-	var imageName = req.body.imageName
-let bucketName = 'gs://poopnet-4fb22.appspot.com'
-let filename = imageName
-const downloadFile = async() => {
-    let destFilename = './views/images/image.png';
-    const options = {
-			destination: destFilename
-		};
-    await storage.bucket(bucketName).file(filename).download(options);
-    res.render('see', {imageLink:'images/image.png'})
-  } 
-downloadFile();
-})
-//hi
-app.post('/create', async (req, res) => {
-	var userData = {
-		username: req.body.username,
-		password: req.body.password,
-		confirmPassword: req.body.confirmPassword
+app.post('/renameImage', async(req, res) => {
+  var renamed = req.body.renamedImage;
+	var newName = req.body.newName;
+	if (renamed == 'start.png' && newName == "") {
+		var g = await functions.retrieveImages(req.app.locals.username)
+		res.render('rename', {message:"", images:Object.keys(g).map(k => g[k]).join('<br><br>')});
 	}
-	if (userData.password == userData.confirmPassword) {
-	var savedData = {
-		username: req.body.username,
-		password: req.body.password,
-		images: {
-			0:""
-		}
-	}
-	db.collection('PDUsers').doc(userData.username).set(savedData);
-	res.writeHead(301, { Location: '/' });
-	res.end("");	
+	else if (renamed == 'start.png' && newName != "") {
+		res.render('rename', {message:"You may not rename start.png!", images:""});
 	}
 	else {
-		res.render('create', {message:"The password's do not match! Pleaase try again."})
+		var file = bucket.file(req.app.locals.username + '/' + renamed);
+		file.rename(req.app.locals.username + '/' + newName, function(err, renamedFile, apiResponse) {});
+		functions.renameImage(req.app.locals.username, renamed, newName)
+		res.render('rename', {message:"Image renamed!", images:""});
+	}
+});
+app.post('/deleteImage', async(req, res) => {
+	var imageDeleted = req.body.imageDelete
+	if (imageDeleted == 'start.png') {
+		var g = await functions.retrieveImages(req.app.locals.username)
+		res.render('delete', {message:"", images:Object.keys(g).map(k => g[k]).join('<br><br>')});
+	}
+	else {
+	var file = bucket.file(req.app.locals.username + '/' + imageDeleted);
+	file.delete(function(err, apiResponse) {});
+	functions.deleteImage(req.app.locals.username, imageDeleted);
+	res.render('delete', {message:"Image deleted successfuly!", images:""});
 	}
 })
-
-app.post('/logIn', async (req, res) => {
-	var username = req.body.username;
-	
 });
